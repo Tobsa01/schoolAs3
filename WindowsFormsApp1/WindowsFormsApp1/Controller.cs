2a3deb7593bf8bc302a0b711259362b332f3e72d
Sandra Densch <s.m.densch@gmail.com>
2020-10-12 18:45:53 +0200

    Einbindung Entity Framework, erste Datenbankabfragen

---
+++ WindowsFormsApp1/WindowsFormsApp1/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Controller
+    {
+        Model model = new Model();
+
+        public void login(string email, string password)
+        {
+            // ToDo: Passwort verschlüsseln
+            List<Users> user = model.select_User_for_Login(email, password);
+            if (user.Count == 0)
+            {
+                // ToDo: Kein Nutzer mit diesen Credentials vorhanden
+            }
+            else if (user.Count == 1)
+            {
+                // ToDo: Login
+            }
+            else
+            {
+                // ToDo: Credentials nicht eindeutig
+            }
+        }
+
+        public void add_User(string lastName, string firstName, string email, int MANumber, string role, string password)
+        {
+            model.insert_User(lastName, firstName, email, MANumber, role, password);
+        }
+    }
+}
