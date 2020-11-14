56808f70a8e17a8e20dbea3fcabecf08fd5d3047
Tobias <tobias0704@hotmail.com>
2020-11-14 23:22:32 +0100

    new querys

---
+++ WindowsFormsApp1/WindowsFormsApp1/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Book
+    {
+        public string Inventar_Number { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Desription { get; set; }
+        public string Condition { get; set; }
+        public string Publisher { get; set; }
+        public Book() { }
+        public Book(string Inventar_Number, string ISBN, string Title, string Author, string Desription, string Condition, string Publisher)
+        {
+            this.Inventar_Number = Inventar_Number;
+            this.ISBN = ISBN;
+            this.Title = Title;
+            this.Author = Author;
+            this.Desription = Desription;
+            this.Condition = Condition;
+            this.Publisher = Publisher;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/QueryObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class QueryObject
+    {
+
+        public static List<T> getWhere<T>(T obj, string tableName)
+        {
+            List<T> result = new List<T>(); 
+            string query = QueryBuilder.buildquery(tableName, obj);
+            List<string[]> list = QueryExecuter.getQueryAsList(query);
+            foreach (string[] l in list)
+            {
+                T t = (T)Activator.CreateInstance(typeof(T));
+                Type prop = typeof(T);
+                PropertyInfo[] propInfos = prop.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                for(int i = 0; i < propInfos.Length; i++)
+                {
+                    propInfos[i].SetValue(t, l[i]);
+                }
+                result.Add(t);
+            };
+            return result;
+        }
+        public static void insertObject<T>(T obj, string tableName)
+        {
+            string query = QueryBuilder.insertquery(tableName, obj);
+            QueryExecuter.insertQuery(query);
+        }
+    }
+}
