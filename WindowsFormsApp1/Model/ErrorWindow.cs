50de4fe81a6a6c89c33a694c373f6084e8aaab7d
Tobias <tobias0704@hotmail.com>
2021-01-07 18:07:23 +0100

    added errorModal

---
+++ WindowsFormsApp1/Model/ErrorWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public static class ErrorWindow
+    {
+        public static void ShowCustomErrorWindow(string errorMessage, string errorTitle, MessageBoxIcon boxIcon, MessageBoxButtons boxButtons)
+        {
+
+            MessageBox.Show(errorMessage, errorTitle,
+                boxButtons, boxIcon);
+        }
+
+        public static void ShowExceptionWindow(this Exception exception)
+        {
+            ShowCustomErrorWindow(exception.Message, "Exception", MessageBoxIcon.Error, MessageBoxButtons.OK);
+        }
+    }
+}
